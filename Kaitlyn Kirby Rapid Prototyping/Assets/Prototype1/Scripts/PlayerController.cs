f196a68338f9ded90c0d07c5ab72f73e42f0205e
K1rby <diabolopersona@gmail.com>
2022-02-19 11:18:24 +1100

    Animations

--- Kaitlyn Kirby Rapid Prototyping/Assets/Prototype1/Scripts/PlayerController.cs
+++ Kaitlyn Kirby Rapid Prototyping/Assets/Prototype1/Scripts/PlayerController.cs
@@ -13,20 +13,24 @@ namespace Prototype1
         public bool isOnGround = true;
         public bool gameOver = false;
 
+        private Animator playerAnim;
+
         // Start is called before the first frame update
         void Start()
         {
             playerRB = GetComponent<Rigidbody>();
+            playerAnim = GetComponent<Animator>();
             Physics.gravity *= gravityModifier;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
             {
                 playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
+                playerAnim.SetTrigger("Jump_trig");
             }
         }
 
@@ -40,6 +44,8 @@ namespace Prototype1
             {
                 gameOver = true;
                 Debug.Log("Game Over");
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
             }
         }
     }
