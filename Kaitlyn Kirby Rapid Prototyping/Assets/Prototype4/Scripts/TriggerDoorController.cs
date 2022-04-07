2a3a3bb91cb907c88a317b0138af5051e538e4e1
K1rby <diabolopersona@gmail.com>
2022-04-07 13:57:32 +1000

    Animated doors open & close

---
+++ Kaitlyn Kirby Rapid Prototyping/Assets/Prototype4/Scripts/TriggerDoorController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator doorAnim = null;
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+
+    [SerializeField] private string doorOpen = "DoorOpenAnim";
+    [SerializeField] private string doorClose = "DoorCloseAnim";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                doorAnim.Play(doorOpen, 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+            else if (closeTrigger)
+            {
+                doorAnim.Play(doorClose, 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
